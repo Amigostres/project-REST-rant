087abaaba83f0bc495d0730065282448946880d1
Jose <josemanuel21302@gmail.com>
2023-03-19 00:11:14 -0700

    adding places view with mock/fake data

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,12 +1,25 @@
-const router = require(`express`).Router()// use .router when making new routes
+const express = require(`express`)
+const router = express.Router()// use .router when making new routes
 
-router.get(`/`, (req, res) => {
-    res.send(`<h1 style = 
-        "color:blue;
-        text-align:center;
-        ">
-        LOCATIONS!!!
-        </h1>`)
-})
+// GET /places
+router.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      },
+       {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('../views/places/index', {places})
+  })
+  
 
 module.exports = router // this will make sure we can use /locations path
--- views/home.jsx
+++ views/home.jsx
@@ -5,9 +5,10 @@ function home() {
     return(
         <Default>
             <main>
-                <h1>HOMESSS</h1>
+                <h1>HOMESSS PAGE! LOL</h1>
             </main>
         </Default>
     )
 }
-module.exports = home
+
+module.exports = home // this is for the res.render(`home`) so that we don't need to get the directory
